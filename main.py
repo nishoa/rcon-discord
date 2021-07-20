60239c3a0bead280442f53f9dd3f96084bdafd67
s1pans <68907038+s1pans@users.noreply.github.com>
2021-07-20 15:03:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from mctools import RCONClient
 from threading import Thread
 
-HOST = 'SERVER-IP'
-PORT = 'SERVER-PORT'
+HOST = os.environ.get('SERVER-IP')
+PORT = os.environ.get('SERVER-PORT')
 print(PORT)
 rcon = RCONClient(HOST, port=int(PORT))
 
