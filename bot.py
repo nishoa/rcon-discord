82856f756ccb76630f1e71fe3007e5a376621c34
s1pans <68907038+s1pans@users.noreply.github.com>
2021-07-20 13:53:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,25 +18,3 @@ async def rcon(ctx, *, text):
     await ctx.send(f'{text} возникла ошибка')
 
 token = os.environ.get('BOT_TOKEN')
-
-# import discord
-# from threading import Thread
-# from main import rc
-#
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print('Logged on as', self.user)
-#
-#     async def on_message(self, message):
-#         # don't respond to ourselves
-#         if message.author == self.user:
-#             return
-#
-#         def mess():
-#             if message:
-#                 mes = message
-#                 return mes
-#                 # await message.channel.send(res)
-#
-# client = MyClient()
-# client.run('ODY2NzQxMTUxNzUzODMwNDMw.YPW9mQ.c9656VfiAvaBWJvcMzytOq7gMZs')
